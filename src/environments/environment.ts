9ca1c1b4eed9b64c6d23127bece2c2dd16632c72
Dmitriy Khvan <dimon.twin@gmail.com>
2023-05-01 18:42:54 +0500

    change dbUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   authUrl: `https://auth.flexit.uz/auth`,
-  // dbUrl: 'https://cc.lendo.uz/api',
-  dbUrl: 'http://10.1.1.177:1680',
-  fileUrl: 'http://dev.lendo.uz/files',
+  dbUrl: 'https://cc.lendo.uz/api',
+  // dbUrl: 'http://10.1.1.177:1680',
+  // fileUrl: 'http://dev.lendo.uz/files',
   // fileUrl: 'http://api.lendo.uz/files',
 };
