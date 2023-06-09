2c82640c03384e785a390dd832b981aec78f5ca5
Dmitriy Khvan <dimon.twin@gmail.com>
2023-06-09 19:35:15 +0500

    accompaniment general info not complete

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,12 +3,12 @@ export const environment = {
   authUrl: `https://auth.flexit.uz/auth`,
 
   // dbUrl: 'http://10.1.1.177:1680',
-  // dbUrl2: 'http://10.1.1.177:5415',
+  dbUrl2: 'http://10.1.1.177:5415',
   // wsUrl: 'ws://10.1.1.177:1680',
 
   dbUrl: 'https://cc.lendo.uz/api',
   // dbUrl2: 'http://10.1.1.14:5415/support/confirm',
-  dbUrl2: 'https://cc.lendo.uz/api2',
+  // dbUrl2: 'https://cc.lendo.uz/api2',
   // dbUrl2: 'http://10.1.1.200:5415/support/confirm',
   wsUrl: 'wss://cc.lendo.uz',
 
