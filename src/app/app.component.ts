56b30014f4a8c192a482e4b4883fbcdf054c9f3d
Dmitriy Khvan <dimon.twin@gmail.com>
2023-06-15 19:29:01 +0500

    auto logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ApiService } from './shared/api/credit.service';
 import { KeycloakService } from 'keycloak-angular';
 import { WebsocketService } from './shared/api/websocket.service';
+import jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +15,20 @@ export class AppComponent {
   constructor(
     // запускает сервисы
     // private apiService: ApiService,
-    // private keycloak: KeycloakService,
+    private keycloak: KeycloakService,
     private webSocketService: WebsocketService
-  ) {}
+  ) {
+    this.keycloak
+      .getToken()
+      .then((token) => {
+        const tokenDecode: any = jwt_decode(token);
+        const time = tokenDecode.exp * 1000 - Date.now();
+        setTimeout(() => {
+          keycloak.logout();
+        }, time);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
 }
